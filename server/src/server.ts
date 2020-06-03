878fd494451fb0d6b24ea641409c3e32247c1674
Uriel <uriel_goncalves@hotmail.com>
2020-06-03 22:01:32 +0100

    Moved routes, knex installed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 //sending json in the request body
 app.use(express.json());
-
-const users = [
-    'Uriel',
-    'Lucas',
-    'Andre',
-    'Victor'
-];
-
-app.get('/users/:id', (request, response)=>{
-    const id = Number(request.params.id);
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.get('/users', (request, response) => {    
-    const search = String(request.query.search);
-
-    const filteredUsers = users.filter(user=>user.includes(search));
-
-    return response.json(filteredUsers);
-});
-
-app.post('/users', (request, response)=>{
-    var data = request.body;
-
-    const user = {
-        name: 'Uriel',
-        email: 'teste@teste.com'
-    };
-
-    return response.json(user);
-});
-
+app.use(routes);
 app.listen(3333);

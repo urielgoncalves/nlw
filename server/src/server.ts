5708532cc7fec8abcb0e39243f05ceb6d1f0c427
Uriel <uriel_goncalves@hotmail.com>
2020-06-03 08:33:36 +0100

    adding routes on back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,40 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuarios');
+//sending json in the request body
+app.use(express.json());
 
-    //response.send('Hello World');
-    response.json([
-        'Uriel',
-        'Lucas',
-        'Victor',
-        'Andre'
-    ]);
+const users = [
+    'Uriel',
+    'Lucas',
+    'Andre',
+    'Victor'
+];
+
+app.get('/users/:id', (request, response)=>{
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.get('/users', (request, response) => {    
+    const search = String(request.query.search);
+
+    const filteredUsers = users.filter(user=>user.includes(search));
+
+    return response.json(filteredUsers);
+});
+
+app.post('/users', (request, response)=>{
+    var data = request.body;
+
+    const user = {
+        name: 'Uriel',
+        email: 'teste@teste.com'
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
